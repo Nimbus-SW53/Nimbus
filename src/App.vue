daea6cbeb9716d8831f3ff43ae6b5e3ade22d9fa
ChrisitanZ <U202011688@upc.edu.pe>
2023-09-02 21:21:01 -0500

    Primer commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Tutorial from "@/components/Tutorial.vue";
+import Category from "@/components/categoria.vue";
+</script>
+
+<template>
+
+  <category />
+
+
+</template>
+
+<style>
+
+
+
+body
+{
+  background-color: blue;
+}
+
+</style>
