2c08364fe925fd70304a39798eac68312630dfd6
farid <u202014468@upc.edu.pe>
2023-09-04 00:38:30 -0500

    providers update

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,14 @@
 import Tutorial from "@/components/Tutorial.vue";
 import Category from "@/components/categoria.vue";
 import Subscriptions from "@/components/Subscriptions.vue";
+import Proveedor from "@/components/Proveedor.vue";
 </script>
 
 <template>
 
   <category />
   <subscriptions/>
-
+  <Proveedor/>
 </template>
 
 <style>
---
+++ src/components/Proveedor.vue
@@ -0,0 +1,39 @@
+<script setup>
+
+</script>
+
+<template>
+<div class ="container">
+  <label for = "proveedor">Elige un proveedor </label>
+  <select id="proveedor" name = "proveedor">
+    <option value = "Agendar Cita">NetQuest </option>
+    <option value = "Agendar Cita">DigitalTech </option>
+    <option value = "Agendar Cita">DigitalSolutions </option>
+
+  </select>
+</div>
+</template>
+
+<script>
+export default {
+  props:['name'],
+  name:"Providers"
+}
+</script>
+
+
+
+
+<style scoped>
+.container{
+  font-family: "Public Sans";
+  margin: 5px;
+  padding: 10px;
+  font-size: 50px;
+  text-align:left;
+  background-color: #4d5c94;
+
+
+}
+
+</style>
